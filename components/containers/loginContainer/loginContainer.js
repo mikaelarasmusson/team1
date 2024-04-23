27944cb283131a0b911db9e7f9e4e41fc2219c63
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2024-04-23 16:50:47 +0200

    added files in index

---
+++ components/containers/loginContainer/loginContainer.js
@@ -0,0 +1,10 @@
+"use strict"
+
+function renderLoginContainer (parentId) {
+    const parent = document.getElementById(parentId);
+    const selfId = "login-container";
+    let dom = document.createElement("div");
+    dom.id = selfId;
+    dom.innerHTML = `
+    `;
+}
