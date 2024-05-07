a985860bd4e875e53b152274c03a2b226f814b8d
Sabka13 <143510385+Sabka13@users.noreply.github.com>
2024-05-07 10:20:20 +0200

    fixed innerHTML  i active filen

--- components/flashcards/playFlashcards/containers/activeFlashcardContainer.js
+++ components/flashcards/playFlashcards/containers/activeFlashcardContainer.js
@@ -1,43 +1,23 @@
-async function renderActiveFlashcardContainer(parentId){
+async function renderActiveFlashcardContainer(parentId) {
   const parent = document.getElementById(parentId);
-  const selfId =  "activeFlashcardContainer";
-  let dom = document.createElement("div");
-  dom.id = selfId;
 
-  const deckSubject = document.createElement("p");
-  deckSubject.classList.add("deckSubject");
-  deckSubject.textContent = "DU1";
-
-  const deckUsername = document.createElement("p");
-  deckUsername.classList.add("deckUsername");
-  deckUsername.textContent = "Sabi13";
+  dom.innerHTML = `
+      <div id = "activeFlahcardContainer">
+      <p class = "deckSubject"> DU1</p>
+      <p class = "deckUsername"> Sabina</p>
+      <h1 class = "question"> Question</h1>
+       </div>
+      `
 
   // alla flashcard
   let flashcards = await getFlashcardInformation();
   // ta reda på vilket val dom gjort (dvs deck)
   let deckIdChoice = State.getEntity("deckIdChoice");
-  
+
   // Ta ut och visa en fråga
 
   // Klicka mellan flera
 
   // hämta en fråga från den valda decken
-
-
-  const question = document.createElement("h1");
-  question.classList.add("question");
-  question.textContent 
-
-  const answer = document.createElement("h1");
-  answer.classList.add("answer");
-  answer.textContent //Hur når vi frågan och svaret på exakt kort.
-  console.log(answer);
-
-  dom.classList.add("activeFlashcards");
-
   parent.append(dom);
-
-
-
-
 }
