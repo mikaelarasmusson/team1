7d8a3b2125f171b4b3ae35381a798c1cf0b07f45
peterssn <linn.sa.petersson@gmail.com>
2024-05-08 11:44:02 +0200

    Added divs and css to editFlashcards

--- components/flashcards/editFlashcards/ContainerRight/editActiveFlashcardContainer.js
+++ components/flashcards/editFlashcards/ContainerRight/editActiveFlashcardContainer.js
@@ -5,14 +5,32 @@ function renderEditActiveFlascardContainer(parentId) {
   dom.id = selfId;
 
   dom.innerHTML = `
-    <div class="postInputContainer">
-    <input class = "editInput" placeholder="Add Question Here"></input>
-    <button id= "postButton" class="controlsButton">Post</button>
+    <div class="subjectNameContainer">
+
+      <div class="nameSubjectPost">
+        <input class="nameSubject" placeholder="Add Subject Here"></input>
+        <button class="nameSubjectButton">Post</button>
+      </div>
+
+      <div class="nameSubjectPost">
+        <input class = "nameSubject" placeholder="Add Name Here"></input>
+        <button class="nameSubjectButton">Post</button>
+      </div>
+
     </div>
 
     <div class="postInputContainer">
-    <input class = "editInput" placeholder="Add Answer Here"></input>
-    <button id= "postButton" class="controlsButton">Post</button>
+
+      <div class ="inputPost">
+        <input class = "editInput" placeholder="Add Question Here"></input>
+        <button id= "postButton" class="controlsButton">Post</button>
+      </div>
+
+      <div class="inputPost">
+        <input class = "editInput" placeholder="Add Answer Here"></input>
+        <button id= "postButton" class="controlsButton">Post</button>
+      </div>
+
     </div>
   `;
 
