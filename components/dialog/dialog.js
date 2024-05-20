da3187a83b64fddc84caef85c2a72ef4849a632e
peterssn <linn.sa.petersson@gmail.com>
2024-05-20 22:59:52 +0200

    Made dialog popup in js and css

---
+++ components/dialog/dialog.js
@@ -0,0 +1,15 @@
+function renderDialog(parentId) {
+  const parent = document.getElementById(parentId);
+  const selfId = "dialog";
+  let dom = document.createElement("dialog");
+  dom.id = selfId;
+
+  dom.innerHTML = `
+    <form id="dialogForm" method="dialog">
+      <p>New card added successfully!</p>
+      <button id="dialogOkButton" >OK</button>
+    </form>
+  `;
+
+  parent.append(dom);
+  };
