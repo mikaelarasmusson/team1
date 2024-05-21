7fd39c1d5e6a9c074f8f9a546b3a20be6c1e4049
mikaelarasmusson <143449047+mikaelarasmusson@users.noreply.github.com>
2024-05-21 11:22:27 +0200

    fixed login

--- components/login/loginContainer/loginContainer.js
+++ components/login/loginContainer/loginContainer.js
@@ -8,10 +8,11 @@ function renderLoginContainer (parentId) {
     dom.innerHTML = `
         <h2>UniConnect</h2>
         <img src="../images/LilacLogotype.jpg" alt="logotype">
-        <input type="text" id="username" placeholder="Username">
-        <input type="password" id="password" placeholder="Password">
+        <input type="text" id="username" placeholder="Username:">
+        <input type="password" id="password" placeholder="Password:">
         <button id="login">Login</button>
         <p>Don't have an account? Sign up <a id="register" href="#">here</a></p>
+        <p>Have you forgotten your password? Change <a id="change-password" href="#">here</a></p>
     `;
 
     //Forgotten password med patch
